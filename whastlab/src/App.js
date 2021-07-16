e44d27b2c658ce222efb859f1a59fd93015d2694
GlaytonVerissimo <glayton.verissimo@hotmail.com>
2021-07-16 14:17:04 -0300

    Estrutura do whatslab

--- whastlab/src/App.js
+++ whastlab/src/App.js
@@ -1,110 +1,51 @@
-import React from "react";
-import Post from "./components/Post/Post";
-import styled from "styled-components";
-
-const arrayPosts = [
-  {
-    nomeUsuario: "Paulinha",
-    fotoUsuario: "https://picsum.photos/50/50",
-    fotoPost: "https://picsum.photos/200/150"
-  },
-  {
-    nomeUsuario: "João",
-    fotoUsuario: "https://picsum.photos/50/51",
-    fotoPost: "https://picsum.photos/200/151"
-  },
-  {
-    nomeUsuario: "Amanda",
-    fotoUsuario: "https://picsum.photos/50/52",
-    fotoPost: "https://picsum.photos/200/152"
-  }
-];
+import React from 'react';
+import './App.css';
+import styled from 'styled-components'
+import { MessageForm } from './components/Mensagem';
 
 const AppContainer = styled.div`
+  max-width: 600px;
+  height: 100vh;
+  border: 1px solid black;
+  flex: 1;
   display: flex;
-  justify-content: center;
   flex-direction: column;
-  align-items: center;
-`;
+`
 
-const FormContainer = styled.div`
+const MessagesContainer = styled.div`
+  flex: 1;
   display: flex;
-  justify-content: space-between;
   flex-direction: column;
-  height: 180px;
-  width: 240px;
-  border: 1px solid gray;
-  margin: 20px;
-  padding: 30px;
-`;
+  justify-content: flex-end;
+  padding: 20px;
+`
+
+const BoldText = styled.span`
+  font-weight: bold;
+`
 
 class App extends React.Component {
-  state = {
-    posts: arrayPosts,
-    userName: "",
-    userPhoto: "",
-    postPhoto: ""
-  };
+  constructor() {
+    super()
 
-  changeUserName = (event) => {
-    this.setState({ userName: event.target.value });
-  };
+    this.state = {
+      messages: []
+    }
+  }
 
-  changeUserPhoto = (event) => {
-    this.setState({ userPhoto: event.target.value });
-  };
-
-  changePostPhoto = (event) => {
-    this.setState({ postPhoto: event.target.value });
-  };
-
-  addNewPost = () => {
-    const newPost = {
-      nomeUsuario: this.state.userName,
-      fotoUsuario: this.state.userPhoto,
-      fotoPost: this.state.postPhoto
-    };
-
-    this.setState({
-      posts: [...this.state.posts, newPost],
-      userName: "",
-      userPhoto: "",
-      postPhoto: ""
-    });
-  };
+  addMessage = (message) => {
+    this.setState({ messages: [...this.state.messages, message] })
+  }
 
   render() {
-    const componentesPost = this.state.posts.map((p) => {
-      return (
-        <Post
-          nomeUsuario={p.nomeUsuario}
-          fotoUsuario={p.fotoUsuario}
-          fotoPost={p.fotoPost}
-        />
-      );
-    });
-
     return (
       <AppContainer>
-        <FormContainer>
-          <input
-            placeholder={"Nome do Usuário"}
-            value={this.state.userName}
-            onChange={this.changeUserName}
-          />
-          <input
-            placeholder={"Foto do Usuário"}
-            value={this.state.userPhoto}
-            onChange={this.changeUserPhoto}
-          />
-          <input
-            placeholder={"Foto do Post"}
-            value={this.state.postPhoto}
-            onChange={this.changePostPhoto}
-          />
-          <button onClick={this.addNewPost}>Criar Post</button>
-        </FormContainer>
-        {componentesPost}
+        <MessagesContainer>
+          {
+            this.state.messages.map((message, index) => <p key={index}><BoldText>{message.user}</BoldText>{': ' + message.text}</p>)
+          }
+        </MessagesContainer>
+        <MessageForm addMessage={this.addMessage} />
       </AppContainer>
     );
   }
