74797644b82952dd364bde88ff1182c788b73ee9
GlaytonVerissimo <glayton.verissimo@hotmail.com>
2021-07-16 16:03:40 -0300

    Pré projeto pronto para adicionar a estilização final

--- whastlab/src/components/Mensagem.js
+++ whastlab/src/components/Mensagem.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import styled from 'styled-components'
+import botaoEnviar from './img/send.png'
+
 
 const MessageFormContainer = styled.div`
   display: flex;
+
+ 
 `
 
 const UserInput = styled.input`
   width: 100px;
+  border-radius: 15px;
+  background-color: LightGrey
 `
 
 const TextInput = styled.input`
   flex: 1;
+  border-radius: 15px;
+  background-color: LightGrey
 `
+const Botao = styled.button`
+  border-radius: 50px;
+  background-color: #a4c639
+`;
+
+
 
 export class MessageForm extends React.Component {
   constructor(props) {
@@ -23,31 +37,35 @@ export class MessageForm extends React.Component {
     }
   }
 
-  onChangeUser = (event) => {
+  onChangeUsuario = (event) => {
     this.setState({userValue: event.target.value})
+    
   }
 
-  onChangeText = (event) => {
+  onChangeTexto = (event) => {
     this.setState({textValue: event.target.value})
   }
 
-  onSendMessage = () => {
-    const message = {
+  onSendMenssagem = () => {
+    const mensagens = {
       user: this.state.userValue,
       text: this.state.textValue
     }
 
-    this.props.addMessage(message)
+    this.props.adicionarMensagem(mensagens)
 
     this.setState({textValue: ''})
+    this.setState({userValue: ''})
   }
+  
+  
 
   render() {
     return (
       <MessageFormContainer>
-        <UserInput type="text" placeholder={'Usuário'} onChange={this.onChangeUser} value={this.state.userValue}/>
-        <TextInput type="text" placeholder={'Mensagem'} onChange={this.onChangeText}  value={this.state.textValue}/>
-        <button onClick={this.onSendMessage}>Enviar</button>
+        <UserInput type="text" placeholder={'Usuário'} onChange={this.onChangeUsuario} value={this.state.userValue}/>
+        <TextInput type="text" placeholder={'Mensagem'} onChange={this.onChangeTexto}  value={this.state.textValue}/>
+        <Botao type="reset" onClick={this.onSendMenssagem}><img src={botaoEnviar}/></Botao>
       </MessageFormContainer>
     );
   }
